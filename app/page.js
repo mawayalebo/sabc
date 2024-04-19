be3a7d52c3261b5c586ba96413b16f37f664162a
mawayalebo <mawayalebo@gmail.com>
2024-04-19 11:16:16 +0200

    changing scroll bar

--- app/page.js
+++ app/page.js
@@ -3,30 +3,7 @@
 export default function Home() {
   return (
     <main className="text-6xl">
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-      <p>1</p>
-
-      
+    
     </main>
   );
 }
